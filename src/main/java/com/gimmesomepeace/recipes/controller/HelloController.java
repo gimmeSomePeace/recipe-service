9f61eb699aa2b78319c2f72a39a26dc389b89db0
admin <ilya_seredkiniam@mail.ru>
2026-02-03 15:02:50 +0500

    Инициализация Spring Boot

---
+++ src/main/java/com/gimmesomepeace/recipes/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.gimmesomepeace.recipes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring boot 4 + Java 21";
+    }
+}
