5107d8568462f0951de78935899eb76afea5edc2
gimmeSomePeace <toadmasterhole@gmail.com>
2026-02-24 21:39:18 +0500

    Поработал с тестами №2

--- src/main/java/com/gimmesomepeace/recipes/controller/AuthController.java
+++ src/main/java/com/gimmesomepeace/recipes/controller/AuthController.java
@@ -2,11 +2,20 @@ package com.gimmesomepeace.recipes.controller;
 
 import com.gimmesomepeace.recipes.dto.request.LoginRequest;
 import com.gimmesomepeace.recipes.dto.request.RegistrationRequest;
+import com.gimmesomepeace.recipes.dto.response.ErrorResponse;
 import com.gimmesomepeace.recipes.dto.response.LoginResponse;
 import com.gimmesomepeace.recipes.dto.response.UserResponse;
 import com.gimmesomepeace.recipes.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Tag(name = "Авторизация", description = "Операции, связанные с авторизацией пользователя")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -21,6 +31,25 @@ public class AuthController {
     @Autowired
     private AuthService service;
 
+    @Operation(summary = "Регистрация", description = "Регистрирует пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Регистрация прошла успешно",
+                    content = @Content(
+                            schema = @Schema(implementation = UserResponse.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Логин уже занят",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+            )
+    })
     @PostMapping("/register")
     ResponseEntity<UserResponse> registration(@RequestBody RegistrationRequest request) {
         UserResponse user = service.register(request);
@@ -29,6 +58,25 @@ public class AuthController {
                 .body(user);
     }
 
+    @Operation(summary = "Авторизация", description = "Авторизирует пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Авторизация прошла успешно",
+                    content = @Content(
+                            schema = @Schema(implementation = LoginResponse.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Неправильный логин или пароль",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+                    )
+            )
+    })
     @PostMapping("/login")
     LoginResponse login(@RequestBody LoginRequest request) {
         return service.login(request);
---
+++ src/test/java/com/gimmesomepeace/recipes/controller/AuthControllerIT.java
@@ -0,0 +1,104 @@
+package com.gimmesomepeace.recipes.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gimmesomepeace.recipes.dto.request.LoginRequest;
+import com.gimmesomepeace.recipes.dto.request.RegistrationRequest;
+import com.gimmesomepeace.recipes.model.Role;
+import com.gimmesomepeace.recipes.model.User;
+import com.gimmesomepeace.recipes.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+class AuthControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void registration_shouldRegisterNewUser() throws Exception {
+        RegistrationRequest registrationRequest = new RegistrationRequest(
+                "name",
+                "login",
+                "some very difficult password"
+        );
+        String json = objectMapper.writeValueAsString(registrationRequest);
+
+        mockMvc.perform(post("/auth/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void registration_shouldFailWhenLoginBusy() throws Exception {
+        User testUser = new User("name", "login", "test password", Role.USER);
+        userRepository.save(testUser);
+
+        RegistrationRequest registrationRequest = new RegistrationRequest(
+                "name",
+                "login",
+                "some very difficult password"
+        );
+        String json = objectMapper.writeValueAsString(registrationRequest);
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void login_shouldLogin() throws Exception {
+
+        User testUser = new User("name", "login", encoder.encode("test password"), Role.USER);
+        userRepository.save(testUser);
+
+        LoginRequest loginRequest = new LoginRequest("login", "test password");
+        String json = objectMapper.writeValueAsString(loginRequest);
+
+        mockMvc.perform(post("/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty());
+    }
+
+    @Test
+    void login_shouldFailWhenWrongPassword() throws Exception {
+        User testUser = new User("name", "login", "test password", Role.USER);
+        userRepository.save(testUser);
+        LoginRequest loginRequest = new LoginRequest("wrong", "test password");
+        String json = objectMapper.writeValueAsString(loginRequest);
+
+        mockMvc.perform(post("/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
+                .andExpect(status().isUnauthorized());
+    }
+}
